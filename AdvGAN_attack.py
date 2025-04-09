9e2b98044efe0400cb1dfccc431db930b4bd44e3
zihuiz <btobmelody007@gmail.com>
2025-04-09 17:50:15 +0800

    Feature: Use GAN,  add boundary distance loss, modify gen_loss and file writing

--- AdvGAN_attack.py
+++ AdvGAN_attack.py
@@ -29,11 +29,25 @@ class AdvGAN_attack:
         self.num = num
 
     def generator_loss(self, d_output):
+        # determine the margin of decision boundary
+        w = self.discriminator.coef_[0]
+        w_norm = np.linalg.norm(w)
+        margin = 2 / w_norm
         # 使用 hinge loss，目標是讓 d_output < 0
-        return tf.reduce_mean(tf.maximum(0.0, d_output + 0.1))  # margin = 0.1
+        return tf.reduce_mean(tf.maximum(0.0, d_output + margin))  # margin = 0.1
 
-    # loss function to influence the perturbation to be as close to 0 as possible
-    def perturb_loss(self, preds, thresh):
+    # loss function to influence the output close to boundary
+    def boundary_distance_loss(self, x):
+        x = tf.reshape(x, (1, -1))
+        # get weigth(w) and bias(b) of discriminator
+        w = self.discriminator.coef_[0]
+        b = self.discriminator.intercept_[0]
+        # 轉為 TensorFlow tensor
+        w_tf = tf.constant(w.reshape(-1, 1), dtype=tf.float32)  # shape: (feature_dim, 1)
+        b_tf = tf.constant(b, dtype=tf.float32)                 # scalar
+        logits = tf.matmul(x, w_tf) + b_tf                 # shape: (batch_size, 1)
+        distance = tf.abs(logits) / tf.norm(w_tf)          # shape: (batch_size, 1)
+        return -tf.reduce_mean(distance)
         return tf.reduce_mean(tf.square(preds))  # L2 範數平方，懲罰過大的微擾
 
     ATTACKED_FEATURES_MIN = [0.0, 2.0, 60.0, 54.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
@@ -43,26 +57,29 @@ class AdvGAN_attack:
     #@tf.function
     def train_step(self, X):
         predict = -1
+        noise = tf.random.normal([1, 100])
         with tf.GradientTape() as gen_tape:
             
             # Use generator to generate perturbation
-            perturbation = self.generator(X)
-            X_perturbed = tf.maximum(X + perturbation, 0)  # 確保 X_perturbed >= 0
-            # X_perturbed = tf.stop_gradient(tf.clip_by_value(X_perturbed, self.ATTACKED_FEATURES_MIN, self.ATTACKED_FEATURES_MAX))
-            # X_perturbed = tf.round(X_perturbed)
-            # X_perturbed = self.ATTACKED_FEATURES_MIN + (self.ATTACKED_FEATURES_MAX - self.ATTACKED_FEATURES_MIN) * tf.tanh(X_perturbed)
-            
+            # perturbation = self.generator(X)
+            # generated_data = tf.maximum(X + perturbation, 0)  # 確保 generated_data >= 0
+            # generated_data = tf.stop_gradient(tf.clip_by_value(generated_data, self.ATTACKED_FEATURES_MIN, self.ATTACKED_FEATURES_MAX))
+            # generated_data = tf.round(generated_data)
+            # generated_data = self.ATTACKED_FEATURES_MIN + (self.ATTACKED_FEATURES_MAX - self.ATTACKED_FEATURES_MIN) * tf.tanh(generated_data)
+            generated_data = self.generator(noise)
+            generated_data = tf.clip_by_value(generated_data, self.ATTACKED_FEATURES_MIN, self.ATTACKED_FEATURES_MAX)
             # 判別器判斷真實和假數據
-            output = self.discriminator.decision_function(X_perturbed)
-            predict = self.discriminator.predict(X_perturbed)
-            # output = self.discriminator.decision_function(X_perturbed)
+            output = self.discriminator.decision_function(generated_data)
+            predict = self.discriminator.predict(generated_data)
+            # output = self.discriminator.decision_function(generated_data)
             # print(f'output:{output}')
 
             # 計算損失
             g_loss = self.generator_loss(output)
-            
-            perturb_loss = self.perturb_loss(perturbation, self.thresh)
-            loss = self.alpha * g_loss + self.beta * perturb_loss
+            bd_loss = self.boundary_distance_loss(generated_data)
+            # perturb_loss = self.perturb_loss(perturbation, self.thresh)
+            # loss = self.alpha * g_loss + self.beta * perturb_loss
+            loss = self.alpha * g_loss + self.beta * bd_loss
 
         # 計算梯度
         gradients_of_generator = gen_tape.gradient(loss, self.generator.trainable_variables)
@@ -73,14 +90,13 @@ class AdvGAN_attack:
 
             # 應用梯度更新模型
         self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
-        return loss, X_perturbed, predict, g_loss, perturb_loss
+        return loss, generated_data, predict, g_loss, bd_loss
 
 
     # 訓練過程
     def train(self, X):
         num_train = f'{self.num}_lr_{self.lr}_a_{self.alpha}_b_{self.beta}_thsh_{self.thresh}'
         # print(f"Start to train data {num_train}----------------------")
-        mode = "w"
         formatted_date = datetime.today().strftime("%m%d")
         # 設定輸出目錄
         output_dir = f"result/{formatted_date}"
@@ -91,24 +107,18 @@ class AdvGAN_attack:
         # 生成完整的輸出檔案路徑
         filename_output = os.path.join(output_dir, f"generated_data_{num_train}.csv")
         # filename_output = f"result/{formatted_date}/generated_data_{num_train}.csv"
-        fields = self.features.append("label")
-        df = pd.DataFrame(X, columns=fields)
-        df.to_csv(filename_output, mode=mode, index=False, encoding="utf-8")
-        
-        filename_losses = os.path.join(output_dir, f"losses_{num_train}.csv")
-        # filename_losses = f"result/{formatted_date}/losses_{num_train}.csv"
-        # file_losses = open(filename_losses, mode, encoding="utf-8")
-        fields_eval = ['loss', 'gen_loss', 'perturb_loss']
-        df_loss = pd.DataFrame([fields_eval])
-        df_loss.to_csv(filename_losses, mode=mode, index=False, encoding="utf-8")
+        fields = self.features
+        fields = fields + ["predict", "loss", "gen_loss", "db_loss"]
+        print(fields)
         mode = "a"
+        
         loss = 0
         generated_data = []
         predict = -999
         for epoch in range(self.epochs):
             # epoch += 1
             # 執行訓練步驟
-            loss, generated_data, predict, g_loss, perturb_loss = self.train_step(X)
+            loss, generated_data, predict, g_loss, bd_loss = self.train_step(X)
 
             # if epoch % 1000 == 0:
             #     print(f'Epoch {epoch}, Loss: {loss:.4f}, G_Loss: {g_loss:.4f}')
@@ -122,15 +132,17 @@ class AdvGAN_attack:
             # transform generated_data and predict to numpy array
             generated_data_np = generated_data.numpy()
             predict_np = np.array(predict, dtype=np.float32).reshape(1, -1)
+            losses_np = np.array([loss, g_loss, bd_loss], dtype=np.float32).reshape(1, -1)
             # merge to shape (1, 10)
             merged_array = np.hstack((generated_data_np, predict_np))
+            merged_array = np.hstack((merged_array, losses_np))
             # 轉換為 DataFrame
-            df = pd.DataFrame(merged_array, columns=fields)
+            df = pd.DataFrame(merged_array)
             # Write to csv
-            df.to_csv(filename_output, mode=mode, header=False, index=False, encoding="utf-8")
-            df_loss = pd.DataFrame([[f'{loss:.4f}', f'{g_loss:.4f}', f'{perturb_loss:.4f}']], columns=fields_eval)
-            df_loss.to_csv(filename_losses, mode=mode, header=False, index=False, encoding="utf-8")
-        pd.DataFrame(predict).to_csv(os.path.join(output_dir, "label.csv"), mode='a', header=False, index=False, encoding="utf-8")
+            df.to_csv(filename_output, mode=mode, header=fields, index=False, encoding="utf-8")
+            if predict == 0:
+                df.to_csv(os.path.join(output_dir, "label_0.csv"), mode='a', header=fields, index=False, encoding="utf-8")
+            fields = False
         # print(f'Epoch {epoch}, Gen Loss: {gen_loss:.4f}')
         # print(f'predict: {predict}')
         # print("----------------------------------------------------------------------")
